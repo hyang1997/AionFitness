f89bf3dd75200706f873cc42daa3baae2c838625
Hao Yang <hao_yang1997@hotmail.com>
2024-08-06 22:41:57 -0400

    removed more oopises

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,15 +11,6 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDGsewAL6eOJiLIaRXRNaY3dAYwFxcKK1Q",
-  authDomain: "aionfitness.firebaseapp.com",
-  projectId: "aionfitness",
-  storageBucket: "aionfitness.appspot.com",
-  messagingSenderId: "10091355590",
-  appId: "1:10091355590:web:a5fbb94bf9727764bde6c7"
-};
-
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
